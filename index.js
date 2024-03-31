12b6502d4df01248658bf0a8cf92d7bf479b233e
Allan Kipkorir <allantuix@gmail.com>
2024-03-31 18:16:25 +0300

    demistified functions scope

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+// Function Declaration
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+
+// Function Expression
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork("work from home"));
+
+// saturdayFun
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+wrapAdjective()();
+console.log(wrapAdjective()());
